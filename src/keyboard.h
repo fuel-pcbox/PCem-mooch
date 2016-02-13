bc7b652f915e2d67cc99beed56c64b6cb2d1f9c0
Darius Goad <alegend45@gmail.com>
2016-02-13 10:36:25 -0600

    Whoops

--- src/keyboard.h
+++ src/keyboard.h
@@ -3,3 +3,9 @@ extern void (*keyboard_poll)();
 extern int keyboard_scan;
 
 extern int pcem_key[272];
+extern uint8_t mode;
+void keyboard_process();
+
+extern int set3_flags[272];
+extern uint8_t set3_all_repeat;
+extern uint8_t set3_all_break;
